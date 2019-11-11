937e6c1cf277aebd3526ad63a7c61ba89adf5445
IulianaIagodka <yuliana.yagodka@gmail.com>
2019-11-11 17:38:32 +0200

    updated

--- blog/models.py
+++ blog/models.py
@@ -2,18 +2,19 @@ from django.db import models
 from django.utils import timezone
 
 
-class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+class Todo(models.Model):
+    #author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
+    completed = models.BooleanField(null=False,default=False)
+    #text = models.TextField()
+    """created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
-            blank=True, null=True)
+            blank=True, null=True)"""
 
-    def publish(self):
+    """def publish(self):
         self.published_date = timezone.now()
-        self.save()
+        self.save()"""
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from models import Todo
+def index(request):
+    todos = Todo.objects.all()
+    context = {
+        'todos':todos
+    }
+    if request.method == 'POST':
+        title = request.POST['title']
+        todo = Todo(title=title)
+        todo.save()
+        return render(request,'index.html',context)
+    else:
+        return render(request,'index.html',context)
 
-# Create your views here.
+def show_completed(request): #show completed task only
+    todos = Todo.objects.filter(completed=True)
+    context = {
+        'todos': todos
+    }
+    return render(request, 'index.html', context)
+
+def show_active(request):   #show active task list
+    todos = Todo.objects.filter(completed=False)
+    context = {
+        'todos': todos
+    }
+    return render(request, 'index.html', context)
+def clear_completed(request):    #Delete the completed tasks
+    Todo.objects.filter(completed=True).delete()
+    todos = Todo.objects.all()
+    context = {
+        'todos': todos
+    }
+    return render(request, 'index.html', context)
+
+def save_state(request):
+     pass
